4d031f29f7d75e11786aa1a1bf12df36aa6eee7c
zipi <zipi@ubunto2.myguest.virtualbox.org>
2023-08-08 10:19:25 +0300

    add question 5, it's work

--- question5.sh
+++ question5.sh
@@ -1,3 +1,26 @@
+  GNU nano 6.2                                        exe5.sh *                                               
+#! /bin/bash
 
 
-try!!!!!
+declare -A matrix
+
+
+function generate_random_number {
+        echo $(( (RANDOM % 100) + 1 ))
+}
+
+
+for ((i=0; i<matrix_size; i++)); do
+        for ((j=0; j<matrix_size; j++)); do
+                matrix[$i,$j]=$(generate_random_number)
+        done
+done
+
+
+for ((i=0; i<matrix_size; i++)); do
+        for ((j=0; j<matrix_size; j++)); do
+                printf "%3d" ${matrix[$i,$j]}
+        done
+        echo
+done
+
