ca040b0e3b7936466d6120346f05155670931458
root <root@Ubunto2.myguest.virtualbox.org>
2023-08-08 14:27:04 +0300

    q6 finish

---
+++ question6.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+declare -A users_matrix
+
+
+# Read system users file
+i=0;
+
+num_columns=6
+#IFS=':' read -a user <<< "$str1" 
+while IFS= read -r line; do
+    IFS=':' read -a user <<< "$line"
+    for ((j=0;j<=num_columns;j++)) do
+        users_matrix[$i,$j]=${user[$j]}
+     done
+    ((i++))
+done < /etc/passwd
+
+echo "after for"
+echo $user_matrix
+for((k=0; k<i; k++)); do
+	for((j=0; j<num_columns; j++)); do
+		printf "%10s\t" ${users_matrix[$k,$j]}
+	done 
+	echo
+done
+
+
+
