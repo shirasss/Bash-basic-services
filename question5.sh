55457e61351d48789294c52c17b51f3d70084832
zipi <zipi@ubunto2.myguest.virtualbox.org>
2023-08-08 10:53:51 +0300

    fix question 5

--- question5.sh
+++ question5.sh
@@ -1,25 +1,30 @@
-  GNU nano 6.2                                        exe5.sh *                                               
 #! /bin/bash
 
 
+
+#declare the array
 declare -A matrix
 
-
+#return random number
 function generate_random_number {
         echo $(( (RANDOM % 100) + 1 ))
 }
 
 
+matrix_size=$(generate_random_number)
+
+#initail the array 
 for ((i=0; i<matrix_size; i++)); do
         for ((j=0; j<matrix_size; j++)); do
                 matrix[$i,$j]=$(generate_random_number)
         done
 done
 
-
+#print the arry
 for ((i=0; i<matrix_size; i++)); do
         for ((j=0; j<matrix_size; j++)); do
-                printf "%3d" ${matrix[$i,$j]}
+#	print in 3d format               
+ printf "%3d" ${matrix[$i,$j]}
         done
         echo
 done
