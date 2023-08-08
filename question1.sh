91c9f376876e4d5a72d0019ce955bae44aa70bbc
shirasss <shirass321@gmail.com>
2023-08-08 09:24:22 +0300

    adding comments to question 1

--- question1.sh
+++ question1.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+#ensures that there are at least 2 arguments
 if [ $# -lt 2 ]; then
     echo "Invalid format of input"
     exit 1
 fi
 
 directory="$1"
-echo $1
+#ensures that the directory exists 
 if [ ! -d "$directory" ]; then
     echo "Directory not found: $directory"
     exit 1
 fi
+#downloads the pictures to the given directory
 for url in "${@:2}"; do
     if wget --no-check-certificate -P "$directory" "$url";then
     #if curl -o "${directory}/$(basename "$url")_$(date +"%Y%m%d_%H%M%S").jpg" -s -L "$url"; then
@@ -19,7 +21,7 @@ for url in "${@:2}"; do
     fi
 done
 
-
+#creating the zip directory
 archive_filename="${directory}_$(date +"%Y%m%d_%H%M%S").zip"
 zip -q -j "$archive_filename" "$directory"/*
 
