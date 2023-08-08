2799a4cd8e87863b9687f5810ed6961e889a6e1b
shirasss <shirass321@gmail.com>
2023-08-08 12:58:29 +0300

    short main

--- main.sh
+++ main.sh
@@ -1,44 +1,10 @@
  #!/bin/bash
 
 choice="$1"
-case "$choice" in
-   -1)
       if [ ! -f question1.sh ]; then
           echo "internal problem,sorry"
           exit 1
       fi
-      ./question1.sh ${@:2}
-    ;;
-   -2)
-      if [ ! -f question2.sh ]; then
-          echo "internal problem,sorry"
-          exit 1
-      fi
-      ./question2.sh ${@:2}
-    ;;
-   -3)
-      if [ ! -f question3.sh ]; then
-          echo "internal problem,sorry"
-          exit 1
-      fi
-      ./question3.sh 
-    ;;
-    -4)
-      if [ ! -f question4.sh ]; then
-          echo "internal problem,sorry"
-          exit 1
-      fi
-      ./question4.sh ${@:2}
-    ;;
-    -5)
-      if [ ! -f question5.sh ]; then
-          echo "internal problem,sorry"
-          exit 1
-      fi
-      ./question5.sh 
-    ;;
-
-
-
-         
-esac
+      ./question$choice.sh ${@:2}
+   
+       
